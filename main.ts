b033bfb28fdf884c277ee5705511f50885304617
Dan Stormont <daniel.stormont@gmail.com>
2022-03-02 17:59:25 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+input.onGesture(Gesture.Shake, function () {
+    roll = randint(1, 6)
+})
+let roll = 0
+basic.showIcon(IconNames.Ghost)
+basic.forever(function () {
+    if (roll == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (roll == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (roll == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (roll == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (roll == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (roll == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
